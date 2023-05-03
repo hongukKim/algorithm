ffadc4c5795c54109ad2b9620bb93a1f2d622135
김홍욱 <106135748+hongukKim@users.noreply.github.com>
2023-05-03 14:18:50 +0900

    [unrated] Title: 무인도 여행, Time: 0.28 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.java
@@ -1,71 +1,63 @@
 import java.util.*;
 
 class Solution {
-    static char map[][];
-    static boolean visited[][];
-    static int dx[] = {1, 0, -1, 0};
-    static int dy[] = {0, 1, 0, -1};
-    static ArrayList<Integer> list = new ArrayList<>();
-
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    
     public int[] solution(String[] maps) {
-        visited = new boolean[maps.length][maps[0].length()];
-        map = new char[maps.length][maps[0].length()];
-
-        for (int a = 0; a < maps.length; a++) {
-            for (int i = 0; i < maps[a].length(); i++) {
-                map[a][i] = maps[a].charAt(i);
-            }
-        }
-
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++) {
-                if (!visited[i][j] && map[i][j] != 'X') {
-
-                    list.add(bfs(i, j, map.length, map[i].length));
+        int n = maps.length;
+        int m = maps[0].length();
+        int[][] board = new int[n][m];
+        boolean[][] isVisited = new boolean[n][m];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (maps[i].charAt(j) != 'X') {
+                    board[i][j] = maps[i].charAt(j) - '0';
                 }
             }
         }
-        int temp[] = list.stream()
-                .mapToInt(Integer::intValue)
-                .sorted()
-                .toArray();
-
-        return temp.length == 0 ? new int[]{-1} : temp;
-    }
-
-    static int bfs(int x, int y, int height, int width) {
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(x, y));
-        visited[x][y] = true;
-        int sum = 0;
-        while (!queue.isEmpty()) {
-            Node now = queue.poll();
-
-            sum += map[now.x][now.y] - '0';
-            for (int i = 0; i < 4; i++) {
-                int nextX = now.x + dx[i];
-                int nextY = now.y + dy[i];
-
-                if (nextX < 0 || nextY < 0 || nextX >= height || nextY >= width) {
-                    continue;
-                } else {
-                    if (!visited[nextX][nextY] && map[nextX][nextY] != 'X') {
-                        queue.add(new Node(nextX, nextY));
-                        visited[nextX][nextY] = true;
-                    }
+        
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!isVisited[i][j] && board[i][j] > 0) {
+                    int cnt = dfs(board, isVisited, i, j, n, m);
+                    ans.add(cnt);
                 }
             }
         }
-        return sum;
+        
+        if (ans.isEmpty()) {
+            ans.add(-1);
+        }
+        
+        Collections.sort(ans);
+        int[] answer = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            answer[i] = ans.get(i);
+        }
+        
+        return answer;
     }
-}
-
-class Node {
-    int x;
-    int y;
-
-    public Node(int x, int y) {
-        this.x = x;
-        this.y = y;
+    
+    static int dfs(int[][] board, boolean[][] isVisited, int x, int y, int n, int m) {
+        isVisited[x][y] = true;
+        int cnt = board[x][y];
+        
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
+                continue;
+            }
+            if (board[nx][ny] == 0 || isVisited[nx][ny]) {
+                continue;
+            }
+            cnt += dfs(board, isVisited, nx, ny, n, m);
+        }
+        
+        return cnt;
     }
 }
