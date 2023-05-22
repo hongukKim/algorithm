b74166c0f37d91a6d41626a61e5eacd99f18a629
김홍욱 <106135748+hongukKim@users.noreply.github.com>
2023-05-22 11:10:57 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int num) {
+         int answer = 0;
+        while (num != 1) {
+            if (num % 2 == 1) {
+                num = num * 3 + 1;
+            } else {
+                num = num / 2;
+            }
+            answer++;
+            if (answer == 500){
+                answer = -1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
