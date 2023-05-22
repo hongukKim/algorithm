f5d171d7403d8cae0627eec92228f36635af7585
김홍욱 <106135748+hongukKim@users.noreply.github.com>
2023-05-22 14:46:31 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int temp = x;
+        
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        
+        return x % sum == 0;
+    }
+}
